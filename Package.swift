fca2c40f8874674e8cb847a1e0050633335e1c2d
Justin Williams <justinw@me.com>
2022-06-26 09:40:40 -0600

    Add macCatalyst as a supported platform

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.5
 
 import PackageDescription
 
@@ -6,11 +6,11 @@ let package = Package(
     name: "JWWCore",
     platforms: [
         .iOS(.v14),
-        .macOS(.v11),
-        .tvOS(.v14),
+        .macCatalyst(.v14),
+        .macOS(.v11)
     ],
     products: [
-        .library(name: "JWWCore", targets: ["JWWCore"]),
+        .library(name: "JWWCore", targets: ["JWWCore"])
     ],
     dependencies: [ ],
     targets: [
