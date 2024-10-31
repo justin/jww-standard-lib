8bbc4fd06081b3a92db977cb1ffbdee5020b5bf1
Justin Williams <justinw@me.com>
2024-10-31 11:12:06 -0600

    Fix available check

--- Sources/JWWCore/OSLog+Extensions.swift
+++ Sources/JWWCore/OSLog+Extensions.swift
@@ -1,7 +1,7 @@
 import Foundation
 import os
 
-@available(OSX 11.0, iOS 14.0, tvOS 14.0, watchOS 7.0, *)
+@available(macOS 11.0, iOS 14.0, tvOS 14.0, watchOS 7.0, *)
 public extension Logger {
     /// Initialize a new `Logger` instance.
     ///
